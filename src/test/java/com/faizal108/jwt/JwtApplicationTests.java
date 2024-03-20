2eb1036ed5441e092c67676cfc396696f5a167c0
faizal108 <69467378+faizal108@users.noreply.github.com>
2024-03-20 23:13:24 +0530

    first commit

---
+++ src/test/java/com/faizal108/jwt/JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faizal108.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
